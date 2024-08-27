7016899f7318bcb2ec60c7da3982ccdc0ef3daf0
Elton-Santos <cooperton4@hotmail.com>
2024-08-27 08:53:50 -0300

    route config

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,15 @@
-import { createBrowserRouter } from 'react-router-dom'
-import Home from './pages/Home'  
+import { createBrowserRouter, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
 
 const router = createBrowserRouter([
     {
         path: '/',
+        element: <Navigate to="/chat-retencao-pme" replace />
+    },
+    {
+        path: '/chat-retencao-pme',
         element: <Home />
-    } 
-])
+    }
+]);
 
-export default router
+export default router;
